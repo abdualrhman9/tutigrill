59d67bc810fbe368a4f58935bc0557acee1c0fab
unknown <abdoalrhman9@hotmail.com>
2022-05-26 15:10:36 +0200

    fix stripe intgreation

--- resources/js/components/PurchseComponent.vue
+++ resources/js/components/PurchseComponent.vue
@@ -1,10 +1,10 @@
 <template>
     <div style="margin-top: 200px">
-      <h1>Purchse Here</h1>
+      <h1>Purchse</h1>
       <StripeCheckout 
         ref="checkoutRef"
         :pk="'pk_test_m6F3plFNeLquMEYTZkpvBlBP00pHDCPGOj'",
-        :sessionId="getSessionId()" 
+        :sessionId="sessionId" 
       />
       <button @click.prevent="submit"> Order </button>
     </div>
@@ -13,20 +13,27 @@
 <script>
 import { StripeCheckout } from '@vue-stripe/vue-stripe'
 export default {
+  mounted(){
+    this.getSessionId();
+  },
   name: 'purchse-component',
   components: {StripeCheckout},
 
   data: function(){
     return {
-
+      sessionId: null,
     }
   },
 
   methods: {
-    getSessionId: function(){
-       axios.get('getSessionId')
-      .then(data=>console.log(data))
+    getSessionId: async function(){
+      let sessionId = await axios.get('getSessionId')
+      .then(resp=>{
+        this.sessionId = resp.data.id
+      })
       .catch(err=>console.log(err));
+
+      return sessionId;
     },
     submit () {
       // You will be redirected to Stripe's secure checkout page
